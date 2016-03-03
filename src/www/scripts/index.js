4ede1823c354281185ff5efb86dfea104cc1893c
wwuyts <wemawuyts@gmail.com>
2016-03-03 14:12:58 +0100

    init

---
+++ src/www/scripts/index.js
@@ -0,0 +1,48 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        document.getElementById('testBtn').addEventListener('click', getImage);
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+
+    function getImage() {
+       var options = {
+          quality: 100,
+          destinationType: Camera.DestinationType.FILE_URI,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: false,
+          encodingType: Camera.EncodingType.JPEG,
+          saveToPhotoAlbum: true,
+          correctOrientation: true
+       };
+       navigator.camera.getPicture(
+          function (data) {
+             var image = document.createElement("img");
+             image.src = data;
+             document.body.appendChild(image);
+          },
+          function (err) {
+             console.log("Error getting image.");
+          },
+          options);
+    }
+
+} )();
